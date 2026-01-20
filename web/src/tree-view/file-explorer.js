66985f713d57dd6a84b8b08b0f3133b030109e69
whysosergious <zergski@gmail.com>
2026-01-20 23:41:45 +0200

    file-tree update

--- web/src/tree-view/file-explorer.js
+++ web/src/tree-view/file-explorer.js
@@ -1,5 +1,25 @@
 import { sh } from "/src/sh.js";
 
+const FOLDER_ICON = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+    <path d="M19.5 21a3 3 0 0 0 3-3V9a3 3 0 0 0-3-3h-5.25a3 3 0 0 1-2.65-1.5L9.9 2.25A3 3 0 0 0 7.25 1.5h-3.75a3 3 0 0 0-3 3v15a3 3 0 0 0 3 3h15.75z" />
+  </svg>
+`;
+
+const FOLDER_OPEN_ICON = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+    <path fill-rule="evenodd" d="M1.5 6a3 3 0 0 1 3-3h3.25a3 3 0 0 1 2.65 1.5l.9 1.5H19.5a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3H4.5a3 3 0 0 1-3-3V6zm3-1.5a1.5 1.5 0 0 0-1.5 1.5v12a1.5 1.5 0 0 0 1.5 1.5h15a1.5 1.5 0 0 0 1.5-1.5V9a1.5 1.5 0 0 0-1.5-1.5h-8.25a1.5 1.5 0 0 1-1.32-.75l-.9-1.5Z" clip-rule="evenodd" />
+  </svg>
+`;
+
+const FILE_ICON = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+    <path d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a.375.375 0 0 1-.375-.375V6.75A3.75 3.75 0 0 0 9 3H5.625zM12.75 12.75a.75.75 0 0 0 0 1.5h2.25a.75.75 0 0 0 0-1.5h-2.25zM12 15a.75.75 0 0 1 .75.75h2.25a.75.75 0 0 1 0 1.5h-2.25a.75.75 0 0 1-.75-.75zM12.75 18.75a.75.75 0 0 0 0 1.5h2.25a.75.75 0 0 0 0-1.5h-2.25z" />
+    <path d="M14.25 9.75a.75.75 0 0 0-1.5 0v1.5c0 .414.336.75.75.75h1.5a.75.75 0 0 0 0-1.5h-.75v-1.5z" />
+    <path d="M15 4.125A2.625 2.625 0 0 0 12.375 1.5h-1.5a.75.75 0 0 0 0 1.5h1.5c.414 0 .75.336.75.75v3a.75.75 0 0 0 .75.75h3a.75.75 0 0 0 .75-.75V6.75A2.625 2.625 0 0 0 15 4.125z" />
+  </svg>
+`;
+
 export class WssFileExplorer extends HTMLElement {
   constructor() {
     super();
@@ -8,20 +28,59 @@ export class WssFileExplorer extends HTMLElement {
       <style>
         :host {
           display: block;
-          font-family: monospace;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+          font-size: 14px;
+          color: #d4d4d4;
+          background-color: #1e1e1e;
+          --icon-color: #c5c5c5;
+          --folder-icon-color: #7aa2f7;
+          --hover-bg-color: rgba(255, 255, 255, 0.1);
         }
         .entry {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          padding: 4px 0px;
           cursor: pointer;
+          user-select: none;
+          transition: background-color 0.1s ease-in-out;
         }
-        .dir {
-          font-weight: bold;
+        .entry:hover {
+          background-color: var(--hover-bg-color);
+        }
+        .entry.file:hover {
+          cursor: default;
+        }
+        .entry-icon {
+          width: 1em;
+          height: 1em;
+          margin-right: 8px;
+          color: var(--icon-color);
+          flex-shrink: 0;
+        }
+        .entry.dir > .entry-icon {
+          color: var(--folder-icon-color);
+        }
+        .entry-name {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
         }
         .children {
-          padding-left: 1em;
+          padding-left: 16px;
           display: none;
         }
         .entry.open > .children {
           display: block;
+        }
+        .entry.open > .entry-header > .entry-icon .closed {
+          display: none;
+        }
+        .entry.open > .entry-header > .entry-icon .open {
+          display: block;
+        }
+         .entry-icon .open {
+          display: none;
         }
       </style>
       <div id="root"></div>
@@ -52,30 +111,59 @@ export class WssFileExplorer extends HTMLElement {
 
   renderFiles(files, parent) {
     parent.innerHTML = "";
-    files.forEach((file) => {
-      const node = this._createNode(file);
-      parent.appendChild(node);
-    });
+    files
+      .sort((a, b) => {
+        if (a.type === b.type) {
+          return a.name.localeCompare(b.name);
+        }
+        return a.type === "dir" ? -1 : 1;
+      })
+      .forEach((file) => {
+        const node = this._createNode(file);
+        parent.appendChild(node);
+      });
   }
 
   _createNode(file, isRoot = false) {
-    const isDir = isRoot || file.name.endsWith("/");
-    const name = isRoot ? file.name : file.name.replace(/\/$/, "");
+    const isDir = isRoot || file.type === "dir";
+    const name = isRoot ? file.name : file.name;
 
     const node = document.createElement("div");
     node.className = "entry " + (isDir ? "dir" : "file");
-    node.textContent = name;
 
     if (isRoot) {
       node.dataset.isRoot = true;
     }
+
+    const entryHeader = document.createElement("div");
+    entryHeader.className = "entry-header";
+    entryHeader.style.display = "flex";
+    entryHeader.style.alignItems = "center";
+
+    const icon = document.createElement("span");
+    icon.className = "entry-icon";
+
+    if (isDir) {
+      icon.innerHTML = `<span class="closed">${FOLDER_ICON}</span><span class="open">${FOLDER_OPEN_ICON}</span>`;
+    } else {
+      icon.innerHTML = FILE_ICON;
+    }
+
+    const nameSpan = document.createElement("span");
+    nameSpan.className = "entry-name";
+    nameSpan.textContent = name;
+
+    entryHeader.appendChild(icon);
+    entryHeader.appendChild(nameSpan);
+
+    node.appendChild(entryHeader);
 
     if (isDir) {
       const children = document.createElement("div");
       children.className = "children";
       node.appendChild(children);
 
-      node.addEventListener("click", async (e) => {
+      entryHeader.addEventListener("click", async (e) => {
         e.stopPropagation();
         if (node.classList.contains("open")) {
           node.classList.remove("open");
@@ -97,11 +185,11 @@ export class WssFileExplorer extends HTMLElement {
     let current = node;
     // special case for the root node
     if (current.dataset.isRoot) {
-      return current.textContent;
+      return current.querySelector(".entry-name").textContent;
     }
 
     while (current && !current.dataset.isRoot) {
-      path.unshift(current.firstChild.textContent);
+      path.unshift(current.querySelector(".entry-name").textContent);
       // ascend to the parent entry, skipping the 'children' container
       const parent = current.parentElement.parentElement;
       if (parent.classList.contains("entry")) {
@@ -114,7 +202,7 @@ export class WssFileExplorer extends HTMLElement {
 
     // now current should be the root node
     if (current && current.dataset.isRoot) {
-      path.unshift(current.textContent);
+      path.unshift(current.querySelector(".entry-name").textContent);
     } else {
       // fallback to the attribute if we couldn't find the root node
       path.unshift(this.getAttribute("root") || "/");
